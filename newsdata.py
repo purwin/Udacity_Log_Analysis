f360431068124d4b2fb2c653ebc6290e59b5b7b9
Michael Purwin <mpurwin@gmail.com>
2018-05-27 20:47:56 -0400

    Define functions

--- newsdata.py
+++ newsdata.py
@@ -1,3 +1,21 @@
-import psycopg2
+# -*- coding: utf-8 -*-
 
-db = psycopg2.connect("dbname=news")
+import psycopg2
+from datetime import datetime
+
+# Your task is to create a reporting tool that prints out reports (in plain text) based on the data in the database.
+
+# 1. What are the most popular three articles of all time?
+# "Princess Shellfish Marries Prince Handsome" — 1201 views
+def pop_articles():
+  pass
+
+# 2. Who are the most popular article authors of all time?
+# Ursula La Multa — 2304 views
+def pop_authors():
+  pass
+
+# 3. On which days did more than 1% of requests lead to errors?
+# July 29, 2016 — 2.5% errors
+def pop_days():
+  pass
